d5dcbf7f3f26e4df70ffddf750b92fb381a422df
Edison Chan <edison@ecwk.sh>
2024-08-18 23:29:32 +0800

    Add health endpoint and PORT config

--- backend/todo/src/app.ts
+++ backend/todo/src/app.ts
@@ -1,11 +1,34 @@
 import express from "express";
+import { sql } from "kysely";
 
-import { Database } from "./types";
+import { db } from "./database";
+import { PORT } from "./config";
 
 export const app = express();
 
-export function main() {
-  app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+export async function main() {
+  if (process.env.NODE_ENV !== "production") {
+    const dotenv = await import("dotenv");
+    dotenv.config();
+  }
+
+  app.get("/health", async (req, res) => {
+    let isDbConnected = false;
+
+    const query = sql`SELECT 1`;
+    try {
+      await query.execute(db);
+    } catch (error) {
+      console.error(error);
+      isDbConnected = false;
+    }
+
+    res.status(200).json({
+      isDbConnected
+    });
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
   });
 }
